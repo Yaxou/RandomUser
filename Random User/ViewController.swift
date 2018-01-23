59c08f9330ac102b7286da46992bb8e82157af79
SUP'Internet 09 <supinternet09@macbook-air-de-supinternet-9.local>
2018-01-23 12:49:49 +0100

    Initial Commit

---
+++ Random User/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Random User
+//
+//  Created by SUP'Internet 09 on 23/01/2018.
+//  Copyright © 2018 SUP'Internet 09. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
